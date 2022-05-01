f8ae279850666d5c781c0ecf5af9f4033e6c8946
nitesh0412 <nitesh90825@gmail.com>
2022-05-01 12:44:09 +0530

    added robots_txt robots_txt to find robot file pf website

---
+++ robots_txt.py
@@ -0,0 +1,11 @@
+import urllib.request
+import io
+
+def get_robots_txt(url):
+    if url.endswith('/'):
+        path = url
+    else:
+        path = url + '/'
+    request = urllib.request.urlopen('https://www.'+ path + 'robots.txt', data=None)
+    return request
+print(get_robots_txt("pharmeasy.in"))
